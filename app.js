e0542be0cccb8cc0480cab7074c2fb0df3ae1acb
Chris K <133260386+chrisjk02@users.noreply.github.com>
2024-02-16 18:40:04 -0800

    Add loop that will generate a 16x16 grid of divs

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const container = document.querySelector('.container');
 const gridItem = document.createElement('div');
+gridItem.style.backgroundColor = 'red';
+gridItem.style.width = '100px';
+gridItem.style.gap = '1rem';
+
+for (let i = 0; i < 16; i++) {
+    container.appendChild(gridItem);
+}
