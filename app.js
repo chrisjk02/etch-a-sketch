d5b3f4f7224a37ec4e077fe8e5654167a50bf6c3
Chris K <133260386+chrisjk02@users.noreply.github.com>
2024-02-16 19:23:53 -0800

    Create second loop to create columns within rows
    Instead of adding 16 individual divs, might be
    better to add 4 rows each containing 4 cols

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const container = document.querySelector('.container');
 
 
-for (let i = 0; i < 16; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    container.appendChild(gridItem);
+for (let i = 0; i < 4; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = 0; j < 4; j++) {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        row.appendChild(column);
+    }
+
+    container.appendChild(row);
 }
