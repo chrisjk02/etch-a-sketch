57ba2cbe75ed32a29b4894e5bd3392a65aa33506
Chris K <133260386+chrisjk02@users.noreply.github.com>
2024-02-17 14:35:51 -0800

    Create 16 squares to make a 16x16 grid instead of 4x4 grid

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('.container');
 
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
 
-    for (let j = 0; j < 4; j++) {
+    for (let j = 0; j < 16; j++) {
         const column = document.createElement('div');
         column.classList.add('column');
         row.appendChild(column);
